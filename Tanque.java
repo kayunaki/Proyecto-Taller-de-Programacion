0f296ecfded8dbb835d38a1e4b73677c1ff676e5
kayunaki <168004407+kayunaki@users.noreply.github.com>
2026-07-09 16:55:16 -0500

    Update Tanque.java

--- Tanque.java
+++ Tanque.java
@@ -2,4 +2,9 @@ public class Tanque extends Enemigo {
  public Tanque(int x, int y) {
         super("Tanque", 6, 2, 1, 2.0, x, y);
     }
+ public void mover(int limiteAncho, int limiteAlto) {
+        this.posicionX += this.velocidad;
+        if (this.posicionX >= limiteAncho || this.posicionX <= 0) {
+            this.velocidad = -this.velocidad;
+        }
 }
