24a16f554f9449896364460cee0a588adea860ea
kayunaki <168004407+kayunaki@users.noreply.github.com>
2026-07-19 23:16:42 -0500

    Update Rapidin class constructor and mover method
    Refactor Rapidin constructor and movement logic to use tablero and jugador.

--- Rapidin.java
+++ Rapidin.java
@@ -1,11 +1,23 @@
 public class Rapidin extends Enemigo {
-    public Rapidin(int x, int y) {
-        super("Rapidin", 2, 9.0, 1, 4.5, x, y);
+    public Rapidin(int fila, int columna) {
+        super("Rapidin", fila, columna, 1);
     }
-    public void mover(int limiteAncho, int limiteAlto) {
-        this.posicionX += (int)this.velocidad;
-        if (this.posicionX > limiteAncho) {
-            this.posicionX = 0;
+    @Override
+    public void mover(Tablero tablero, Jugador jugador) {
+        if (!activo) return;
+        for (int i = 0; i < 2; i++) {
+            int df = jugador.fila - fila;
+            int dc = jugador.columna - columna;
+            int nf = fila, nc = columna;
+            if (Math.abs(df) > Math.abs(dc)) {
+                if (df < 0) nf--; else if (df > 0) nf++;
+            } else {
+                if (dc < 0) nc--; else if (dc > 0) nc++;
+            }
+            if (tablero.esMovimientoValido(nf, nc)) {
+                fila = nf;
+                columna = nc;
+            }
         }
-}
+    }
 }
