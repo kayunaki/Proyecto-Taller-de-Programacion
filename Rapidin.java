deb8adf231c8ab16fd5e35a97f2e63e3ed4fc614
kayunaki <168004407+kayunaki@users.noreply.github.com>
2026-07-19 21:08:51 -0500

    Fix constructor argument type and update mover method

--- Rapidin.java
+++ Rapidin.java
@@ -1,9 +1,9 @@
 public class Rapidin extends Enemigo {
     public Rapidin(int x, int y) {
-        super("Rapidin", 2, 9, 1, 4.5, x, y);
+        super("Rapidin", 2, 9.0, 1, 4.5, x, y);
     }
     public void mover(int limiteAncho, int limiteAlto) {
-        this.posicionX += this.velocidad;
+        this.posicionX += (int)this.velocidad;
         if (this.posicionX > limiteAncho) {
             this.posicionX = 0;
         }
