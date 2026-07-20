12069147f18a5650570f885f166a574d42ea7c4e
kayunaki <168004407+kayunaki@users.noreply.github.com>
2026-07-19 21:07:26 -0500

    Refactor methods for damage handling and movement
    Refactor recibirDañoYDividir to return a Multiple instance and update mover method to cast velocidad to int.

--- Multiple.java
+++ Multiple.java
@@ -1,30 +1,24 @@
 public class Multiple extends Enemigo {
     private int division;
-    private boolean seDividio EnEsteTurno = false; 
+    private boolean seDividioEnEsteTurno = false; 
     public Multiple(int X, int Y, int division) {
-        super("Multiple", 4, 5, 1, 3.0, X, Y);
+        super("Multiple", 4, 5.0, 1, 3.0, X, Y);
         this.division = division; 
     }
-    public void recibirDaño(int cantidad) {
-        this.vida -= cantidad;
+    public Multiple recibirDañoYDividir(int cantidad) {
+        super.recibirDaño(cantidad);
         if (this.vida < 0) {
             this.vida = 0;
-        }
+        } 
         if (this.vida > 0 && this.division > 0) {
-            this.seDividioEnEsteTurno = true;
-        }
-    }
-    public Multiple obtenerDivision() {
-        if (this.seDividioEnEsteTurno) {
-            this.seDividioEnEsteTurno = false;
             this.division--;
             return new Multiple(this.posicionX + 15, this.posicionY + 15, this.division);
         }
         return null;
     }
     public void mover(int limiteAncho, int limiteAlto) {
-        this.posicionX += this.velocidad;
-        this.posicionY += this.velocidad;
+        this.posicionX += (int)this.velocidad;
+        this.posicionY += (int)this.velocidad;
         if (this.posicionX > limiteAncho) this.posicionX = 0;
         if (this.posicionY > limiteAlto) this.posicionY = 0;
     }
