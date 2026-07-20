58bd2508f2ec37dc582e7f34acaec0a52b8e91a3
kayunaki <168004407+kayunaki@users.noreply.github.com>
2026-07-19 23:06:25 -0500

    Refactor ControladorEnemigos to use array instead of list

--- ControladorEnemigos.java
+++ ControladorEnemigos.java
@@ -1,70 +1,31 @@
-import java.util.ArrayList;
-import java.util.List;
 public class ControladorEnemigos {
-    private List<Enemigos> enemigos;
-    private List<Enemigo> enemigosParaCrear;
-    private Jugador jugador;
-    public ControladorEnemigos(Jugador jugador) {
-        this.enemigos = new ArrayList<>();
-        this.enemigosParaCrear = new ArrayList<>();
-        this.jugador = jugador;
+    private Enemigo[] enemigos;
+    public ControladorEnemigos(Enemigo[] enemigos) {
+        this.enemigos = enemigos;
     }
-     public void generarEnemigosPorTamañoMapa(int anchoMapa, int altoMapa) {
-        int areaTotal = anchoMapa * altoMapa;
-        if (areaTotal > 400) { 
-        enemigos.add(new Fantasma(1, altoMapa / 2, 1, 0));
-        enemigos.add(new Rapidin(anchoMapa - 2, altoMapa - 2));
-        enemigos.add(new Tanque(2, 2));
-        enemigos.add(new Multiple(anchoMapa / 2, altoMapa - 2, 2));
-    } else {
-        enemigos.add(new Tanque(2, 2));
-        enemigos.add(new Multiple(anchoMapa - 2, altoMapa - 2, 2));
-    }
-     }
-    public void moverEnemigos(int anchoMapa, int altoMapa) {
-        generarMovimientos();
-        for (Enemigo e : enemigos) {
-            e.mover(anchoMapa, altoMapa);
-        }
-    }
-    if (!enemigosParaAgregar.isEmpty()) {
-        enemigos.addAll(enemigosParaAgregar);
-        enemigosParaAgregar.clear();
-    }
-    public void registrarDañoAEnemigo(Enemigo e, int cantidad) {
-        if (e instanceof Multiple) {
-            Multiple clon = ((Multiple) e).recibirDañoYDividir(cantidad);
-            if (clon != null) {
-                this.agregarEnemigo(clon); 
-            }
-        } else {
-            e.recibirDaño(cantidad);
-        }
-    }
-    public void verificarColisiones() {
-        int jugadorX = jugador.getPosicionX();
-        int jugadorY = jugador.getPosicionY();
-        for (Enemigo e : enemigos) {
-            int daño = e.realizarDaño(jugadorX, jugadorY);
-            if (daño > 0) {
-                jugador.recibirDaño(daño);
+    public void moverEnemigos(Tablero tablero, Jugador jugador) {
+        for (int i = 0; i < enemigos.length; i++) {
+            if (enemigos[i] != null && enemigos[i].isActivo()) {
+                enemigos[i].mover(tablero, jugador);
             }
         }
     }
-    public void eliminarEnemigosInactivos() {
-        enemigos.removeIf(e -> e.getVida() <= 0);
-    }
-    public void generarMovimientos() {
-        int jugadorX = jugador.getPosicionX();
-        int jugadorY = jugador.getPosicionY();
-        for (Enemigo e : enemigos) {
-            e.calcularEstado(jugadorX, jugadorY);
+    public void verificarColisiones(Jugador jugador) {
+        for (int i = 0; i < enemigos.length; i++) {
+            if (enemigos[i] != null && enemigos[i].isActivo()) {
+                if (enemigos[i].verificarColision(jugador)) {
+                    if (jugador.isPoderActivo()) {
+                        enemigos[i].setActivo(false);
+                        System.out.println("\n\u001B[1;32m¡Has cazado a un " + enemigos[i].getTipo() + " gracias a tu poder!\u001B[0m");
+                        jugador.recogerPunto(50);
+                    } else {
+                        enemigos[i].atacar(jugador);
+                    }
+                }
+            }
         }
     }
-    public void agregarEnemigo(Enemigo e) {
-        this.enemigos.add(e);
-    }
-    public List<Enemigo> getEnemigos() {
+    public Enemigo[] getEnemigos() {
         return enemigos;
     }
 }
